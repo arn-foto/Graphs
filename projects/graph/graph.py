eba5c845c4fce83dae9cf2802b01cae236f36b0b
Anthony N <foto.arn@gmail.com>
2020-10-06 14:50:23 -0700

    + day 1 mvp

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -11,9 +11,9 @@ class Graph:
 
     def add_vertex(self, vertex_id): 
         self.vertices[vertex_id] = set()
-        """
-        Add a vertex to the graph.
-        """
+        # """
+        # Add a vertex to the graph.
+        # """
         
     def add_edge(self, v1, v2):
         self.vertices[v1].add(v2)
@@ -25,6 +25,8 @@ class Graph:
     def get_neighbors(self, vertex_id):
         return self.vertices[vertex_id]
         
+
+
 
     def bft(self, starting_vertex):
         q = Queue()
@@ -44,6 +46,8 @@ class Graph:
         # Print each vertex in breadth-first order
         # beginning from starting_vertex.
         # """
+      
+      
         
     def dft(self, starting_vertex):
         q = Stack()
@@ -63,11 +67,12 @@ class Graph:
         # beginning from starting_vertex.
         # """
 
+
+
     def dft_recursive(self, starting_vertex, visited=None):
         # instantiate if needed
         if visited is None:
             visited = set()
-
         # start
         if starting_vertex not in visited:
             visited.add(starting_vertex)
@@ -82,51 +87,68 @@ class Graph:
         # This should be done using recursion.
         # """
         
-    def bfs(self, starting_vertex, destination_vertex):
-        q = Queue()
-        visited = set()
         
-        q.enqueue([starting_vertex])
-        while q.size() > 0:
-            path = q.dequeue()
-            v = path[-1]
-            if v not in visited:
-                if v == destination_vertex:
-                    return path  # Found it!
-                visited.add(v)
-                for neighbor in self.get_neighbors(v):
-                    new_path = path + [neighbor]
-                    q.enqueue(new_path)
-                    return None
-
+        
+    def bfs(self, starting_vertex, destination_vertex):
+        # Create an empty queue
+        path = Queue()
+        # Add the starting vertex to the path
+        path.enqueue([starting_vertex])
+        # Create a Set to store visited vertices
+        visited = set()
+        # if queue is not empty
+        while path.size() > 0:
+            # dequeue the first path 
+            new_path = path.dequeue()
+            # grab the last vertext from
+            edge = new_path[-1]
+            # if the vertext has not been visited
+            if edge not in visited:
+                # check to see if this is the target
+                if edge is destination_vertex:
+                    # if so then return path
+                    return new_path
+                visited.add(edge)
+                # add a path to its neighbor to the back of the stack
+                neighbors = self.get_neighbors(edge)
+                for neighbor in neighbors:
+                    # Copy the path
+                    path_copy = new_path.copy()
+                    # append neighbor to the back
+                    path_copy.append(neighbor)
+                    path.enqueue(path_copy)
         # """
         # Return a list containing the shortest path from
         # starting_vertex to destination_vertex in
         # breath-first order.
         # """
 
-    def dfs(self, starting_vertex, destination_vertex):
-        s = Stack()
 
-        s.push([starting_vertex])
+
+    def dfs(self, starting_vertex, destination_vertex):
+        # Create an empty stack
+        path = Stack()
+        # Add the starting vertex to the path
+        path.push(starting_vertex)
+        # Create a Set to store visited vertices
         visited = set()
-        
-        while s.size() > 0:
-            
-            currPath = s.pop()
-            
-            currNode = currPath[-1] 
-            
-            if currNode == destination_vertex:
-                return currPath
-            
-            if currNode not in visited:
-                visited.add(currNode)
-                
-                for neighbor in self.get_neighbors(currNode):
-                    new_Path = list(currPath)
-                    new_Path.append(neighbor)
-                    s.push(new_Path)
+        #  Add the starting vertex to the set
+        visited.add(starting_vertex)
+        # Loop while the stack is not empty
+        while path.size() > 0:
+            # Pop the first element
+            new_edge = path.pop()
+            # Add the popped element to the visited set
+            visited.add(new_edge)
+            # Get all neighbors
+            neighbors = self.get_neighbors(new_edge)
+           # Put all the neighbors on the stack
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                   path.push(neighbor)
+                if neighbor is destination_vertex:
+                    visited.add(neighbor)
+                    return list(visited)
 
         # """
         # Return a list containing a path from
